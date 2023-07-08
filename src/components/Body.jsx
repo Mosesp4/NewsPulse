fd2619573f975ed0d9432af2df09751a025c10c2
Moses Echela <echelachris@gmail.com>
2023-07-08 13:51:35 +0100

    update responsiveness

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,9 +6,9 @@ const Body = () => {
 	text-white overflow-hidden justify-between align-middle">
       
       <main className="container mx-auto px-4 sm:px-4 pt-6 sm:pt-16 flex-1 text-center">
-        <h2 className="text-1xl sm:text-4xl md:text-4xl lg:text-4xl ">Welcome to Mac Chris Services</h2>
-        <h1 className="text-1xl sm:text-5xl md:text-5xl lg:text-8xl uppercase 
-		font-black mb-3">
+        <h2 className="text-xl sm:text-4xl md:text-4xl lg:text-4xl ">Welcome to Mac Chris Services</h2>
+        <h1 className="text-2xl sm:text-5xl md:text-5xl lg:text-8xl uppercase 
+		font-black mb-4">
           Top-Notch Services tailored to meet your unique needs.
         </h1>
 
