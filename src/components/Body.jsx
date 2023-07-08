2a562d0fc6751a909c2d86fdbb63a438ceba7b75
Moses Echela <echelachris@gmail.com>
2023-07-08 13:56:57 +0100

    mailchimp T&C

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,7 +6,7 @@ const Body = () => {
 	text-white overflow-hidden justify-between align-middle">
       
       <main className="container mx-auto px-4 sm:px-4 pt-6 sm:pt-16 flex-1 text-center">
-        <h2 className="text-xl sm:text-4xl md:text-4xl lg:text-4xl ">Welcome to Mac Chris Services</h2>
+        <h2 className="text-xl sm:text-4xl md:text-4xl lg:text-4xl ">Welcome to Mac-Chris Services</h2>
         <h1 className="text-2xl sm:text-5xl md:text-5xl lg:text-8xl uppercase 
 		font-black mb-4">
           Top-Notch Services tailored to meet your unique needs.
@@ -48,7 +48,7 @@ const Body = () => {
           </div>
 
           <div className="opacity-75 italic">
-            By subscribing, you agree with Revue's{' '}
+            By subscribing, you agree with mailchimp{' '}
             <a target="_blank" href="https://www.getrevue.co/terms" className="hover:opacity-80 duration-150">
               Terms of Service
             </a>{' '}
